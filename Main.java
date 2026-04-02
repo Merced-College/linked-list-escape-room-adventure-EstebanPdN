280a52f86279c36e5fda7d3d8d172c18f422dab7
EstebanPdN <72305261+EstebanPdN@users.noreply.github.com>
2026-04-02 17:17:49 +0000

    Test that project runs correctly in console
    Also added comment with my name

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/*
+ * Name: Esteban Pereira das Neves
+ * Course: CPSC-39
+ * Project: Escape Room Adventure
+ */
+
 public class Main {
     public static void main(String[] args) {
         AdventureGame game = new AdventureGame();
